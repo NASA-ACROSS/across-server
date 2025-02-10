5bbb62e3239efcf95cc9887247ab9b4a2cbaad33
Jamie Kennea <kennea@swift.psu.edu>
2025-02-10 16:04:21 -0500

    fix(tle): Updates tests and seeds based on PR comments
    Remove other patch
    
    fix(migration): Added missing primary_key definitions to TLE migration

--- tests/tle/router_test.py
+++ tests/tle/router_test.py
@@ -4,15 +4,16 @@ import pytest_asyncio
 from httpx import AsyncClient
 
 
-class TestTLEPostRoute:
-    @pytest_asyncio.fixture(autouse=True)
-    async def setup(self, async_client: AsyncClient, mock_tle_data: dict):
-        self.client = async_client
-        self.endpoint = "/tle/"
-        self.data = mock_tle_data
+class TestTLE:
+    class TestPost:
+        @pytest_asyncio.fixture(autouse=True)
+        async def setup(self, async_client: AsyncClient, mock_tle_data: dict):
+            self.client = async_client
+            self.endpoint = "/tle/"
+            self.data = mock_tle_data
 
-    @pytest.mark.asyncio
-    async def test_should_return_201_when_tle_is_created(self):
-        """Should return a 201 when a new tle is successfully created"""
-        res = await self.client.post(self.endpoint, json=self.data)
-        assert res.status_code == fastapi.status.HTTP_201_CREATED
+        @pytest.mark.asyncio
+        async def test_should_return_201_when_tle_is_created(self):
+            """Should return a 201 when a new tle is successfully created"""
+            res = await self.client.post(self.endpoint, json=self.data)
+            assert res.status_code == fastapi.status.HTTP_201_CREATED
--- tests/tle/service_test.py
+++ tests/tle/service_test.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock
 
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,29 +11,27 @@ from across_server.routes.tle.service import TLEService
 
 class TestTLEService:
     @pytest.mark.asyncio
-    @patch.object(TLEService, "exists", new_callable=AsyncMock)
-    async def test_create_should_return_tle_when_successful(
-        self, mock_exists: AsyncMock, mock_db: AsyncSession, mock_tle: schemas.TLECreate
+    async def test_should_return_tle_when_create_successful(
+        self, mock_result: AsyncMock, mock_db: AsyncSession, mock_tle: schemas.TLECreate
     ) -> None:
         """Should return the tle when successful"""
-        mock_exists.return_value = False
+        mock_result.scalar_one_or_none.return_value = None
         service = TLEService(mock_db)
         tle = await service.create(mock_tle)
         assert isinstance(tle, TLE)
 
     @pytest.mark.asyncio
-    async def test_tle_service_exists(mock_tle_service, mock_db):
+    async def test_should_return_true_when_tle_exists(mock_tle_service, mock_db):
         mock_tle_service = TLEService(mock_db)
         exists = await mock_tle_service.exists(norad_id=12345, epoch=datetime.now())
         assert exists is True
 
     @pytest.mark.asyncio
-    @patch.object(TLEService, "exists", new_callable=AsyncMock)
     async def test_create_should_save_tle_to_database(
-        self, mock_exists: AsyncMock, mock_db: AsyncSession, mock_tle: schemas.TLECreate
+        self, mock_result: AsyncMock, mock_db: AsyncSession, mock_tle: schemas.TLECreate
     ) -> None:
         """Should save the tle to the database when successful"""
-        mock_exists.return_value = False
+        mock_result.scalar_one_or_none.return_value = None
         service = TLEService(mock_db)
         await service.create(mock_tle)
 
