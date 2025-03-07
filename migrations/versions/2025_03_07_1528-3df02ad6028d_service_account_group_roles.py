bdc535b3dfb51733c1e70d9e88c925815764b305
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2025-03-07 15:30:01 -0500

    chore(migration): fix migration ref and date to make it chronological

--- migrations/versions/2025_03_07_1528-3df02ad6028d_service_account_group_roles.py
+++ migrations/versions/2025_03_07_1528-3df02ad6028d_service_account_group_roles.py
@@ -1,8 +1,8 @@
 """service account group roles
 
 Revision ID: 3df02ad6028d
-Revises: 818523f50d93
-Create Date: 2025-02-18 15:28:39.513808
+Revises: 3245c430ace2
+Create Date: 2025-03-07 15:28:39.513808
 
 """
 
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "3df02ad6028d"
-down_revision: Union[str, None] = "818523f50d93"
+down_revision: Union[str, None] = "3245c430ace2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
