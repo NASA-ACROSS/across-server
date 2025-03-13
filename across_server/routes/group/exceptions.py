7d9c3311a47e1960113a9fda63f98a4a89696037
Nitzan Frock <nitzanfrock@gmail.com>
2025-03-13 11:01:24 -0700

    force lint error

--- across_server/routes/group/exceptions.py
+++ across_server/routes/group/exceptions.py
@@ -6,3 +6,6 @@ from ...core.exceptions import NotFoundException
 class GroupNotFoundException(NotFoundException):
     def __init__(self, id: uuid.UUID):
         super().__init__(entity_name="Group", entity_id=id)
+
+
+test
