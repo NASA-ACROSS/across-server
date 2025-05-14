9dcf53dfe9cf710f0b677f28292ad3f6c66f7d72
Nitzan Frock <33351056+nitzan-frock@users.noreply.github.com>
2025-05-14 12:28:08 -0400

    feat(db): convert necessary seeds into migrations for deployment (#227)
    * feat(db): init data migrataions for perms and roles
    
    * feat(db): seeding uses hard-coded UUIDs and session.merge along with migration data
    
    ---------
    
    Co-authored-by: Nitzan Frock <nitzan.n.frock@nasa.gov>

--- migrations/seeds/instruments.py
+++ migrations/seeds/instruments.py
@@ -35,8 +35,4 @@ sandy_optical_instrument = Instrument(
     is_operational=True,
 )
 
-instruments = [
-    sandy_instrument_calorimeter,
-    sandy_all_sky_instrument,
-    sandy_optical_instrument,
-]
+instruments = [sandy_instrument_calorimeter, sandy_all_sky_instrument, sandy_optical_instrument]
