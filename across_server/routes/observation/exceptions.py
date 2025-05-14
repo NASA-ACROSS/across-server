b284299bda1d346e8a08b64034c3b0c310c0a9d5
swyatt7 <swyatt@email.arizona.edu>
2025-05-14 12:07:31 -0700

    final testing

--- across_server/routes/observation/exceptions.py
+++ across_server/routes/observation/exceptions.py
@@ -1,8 +1,6 @@
 import uuid
 
-from fastapi import status
-
-from ...core.exceptions import AcrossHTTPException, NotFoundException
+from ...core.exceptions import InvalidEntityException, NotFoundException
 
 
 class ObservationNotFoundException(NotFoundException):
@@ -10,10 +8,6 @@ class ObservationNotFoundException(NotFoundException):
         super().__init__(entity_name="Observation", entity_id=observation_id)
 
 
-class InvalidObservationReadParametersException(AcrossHTTPException):
+class InvalidObservationReadParametersException(InvalidEntityException):
     def __init__(self, message: str):
-        super().__init__(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            message=message,
-            log_data={},
-        )
+        super().__init__(entity_name="ObservationRead", message=message)
