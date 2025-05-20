f6bb92afdafbc1fc5526c45865ed3cb50cce1906
Craig Pellegrino <42218592+crpellegrino@users.noreply.github.com>
2025-05-20 10:20:47 -0400

    task: add fermi observatory migration (#221)
    * task: add fermi observatory migration
    
    * fix: address review comments, add scintillator instrument type
    
    * fix: add reference for Fermi bandpass info
    
    * task: add fermi uuids, group role, adjust migration filename
    
    * task: adding Fermi instrument FOVs
    
    * fix: use InstrumentFOV enum values in migration
    
    * fix(review): adjust algorithm to match ixpe migration
    
    * fix(review): change down revision after rebase
    
    * fix(review): rename migration to keep order correct
    
    * fix(review): hard code UUIDs, move group and group role info into observatory dictionary

---
+++ migrations/versions/_2025_05_19_1515-7f001eadcb4a_add_fermi.py
@@ -0,0 +1,301 @@
+"""add fermi
+
+Revision ID: 7f001eadcb4a
+Revises: a6590318e932
+Create Date: 2025-05-19 15:15:20.099908
+
+"""
+
+import uuid
+from typing import Sequence, Union
+
+import numpy as np
+from across.tools import EnergyBandpass, convert_to_wave
+from across.tools.core import enums as tools_enums
+from alembic import op
+from sqlalchemy import orm, select
+
+from across_server.core.enums.ephemeris_type import EphemerisType
+from across_server.core.enums.instrument_fov import InstrumentFOV
+from across_server.core.enums.instrument_type import InstrumentType
+from migrations.db_util import ACROSSFootprintPoint, create_geography
+from migrations.versions.model_snapshots.models_2025_05_15 import (
+    EarthLocationParameters,
+    Filter,
+    Footprint,
+    Group,
+    GroupRole,
+    Instrument,
+    JPLEphemerisParameters,
+    Observatory,
+    ObservatoryEphemerisType,
+    Permission,
+    SpiceKernelParameters,
+    Telescope,
+    TLEParameters,
+)
+
+# revision identifiers, used by Alembic.
+revision: str = "7f001eadcb4a"
+down_revision: Union[str, None] = "a6590318e932"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+# LAT has a FOV of 2.4 steradians
+radius = 50  # degrees
+thetas = np.linspace(0, -2 * np.pi, 200)
+ras = radius * np.cos(thetas)
+decs = radius * np.sin(thetas)
+LAT_FOOTPRINT = [[{"x": ras[i], "y": decs[i]} for i in range(len((ras)))]]
+
+# Info about LAT and GBM bandpasses found here:
+# https://heasarc.gsfc.nasa.gov/docs/heasarc/missions/fermi.html
+LAT_ENERGY_BANDPASS = EnergyBandpass(
+    min=0.02,
+    max=300,
+    unit=tools_enums.EnergyUnit.GeV,
+)
+LAT_WAVELENGTH_BANDPASS = convert_to_wave(LAT_ENERGY_BANDPASS)
+
+GBM_ENERGY_BANDPASS = EnergyBandpass(
+    min=0.008,
+    max=40,
+    unit=tools_enums.EnergyUnit.MeV,
+)
+GBM_WAVELENGTH_BANDPASS = convert_to_wave(GBM_ENERGY_BANDPASS)
+
+OBSERVATORY = {
+    "name": "Fermi Gamma-ray Space Telescope",
+    "short_name": "Fermi",
+    "id": uuid.UUID("3417942b-7cc9-4e91-a9ab-fbc800871ef5"),
+    "type": "SPACE_BASED",
+    "reference_url": "https://fermi.gsfc.nasa.gov",
+    "is_operational": True,
+    "telescopes": [
+        {
+            "name": "Large Area Telescope",
+            "short_name": "LAT",
+            "id": uuid.UUID("222d5f4a-8fd6-4299-a696-b3a6cb13d7bc"),
+            "is_operational": True,
+            "reference_url": "https://fermi.gsfc.nasa.gov",
+            "instruments": [
+                {
+                    "name": "Large Area Telescope",
+                    "short_name": "LAT",
+                    "id": uuid.UUID("7f7b97ed-e341-476f-908b-7bad8740b31b"),
+                    "field_of_view": InstrumentFOV.POLYGON.value,
+                    "footprint": LAT_FOOTPRINT,
+                    "is_operational": True,
+                    "reference_url": "https://heasarc.gsfc.nasa.gov/docs/heasarc/missions/fermi.html",
+                    "type": InstrumentType.CALORIMETER.value,
+                    "filters": [
+                        {
+                            "name": "Fermi LAT Bandpass",
+                            "id": uuid.UUID("415f0c75-111e-4970-b89d-b55cc5975a81"),
+                            "min_wavelength": LAT_WAVELENGTH_BANDPASS.min,
+                            "max_wavelength": LAT_WAVELENGTH_BANDPASS.max,
+                            "is_operational": True,
+                            "reference_url": "https://heasarc.gsfc.nasa.gov/docs/heasarc/missions/fermi.html",
+                        }
+                    ],
+                },
+            ],
+        },
+        {
+            "name": "Gamma-ray Burst Monitor",
+            "short_name": "GBM",
+            "id": uuid.UUID("6fd90826-c3c9-4ad0-be01-63ed88a8f484"),
+            "is_operational": True,
+            "reference_url": "https://fermi.gsfc.nasa.gov",
+            "instruments": [
+                {
+                    "name": "Gamma-ray Burst Monitor",
+                    "short_name": "GBM",
+                    "id": uuid.UUID("1c80c356-dc44-404f-aeba-060097a7a59d"),
+                    "field_of_view": InstrumentFOV.ALL_SKY.value,
+                    "footprint": [],
+                    "is_operational": True,
+                    "reference_url": "https://heasarc.gsfc.nasa.gov/docs/heasarc/missions/fermi.html",
+                    "type": InstrumentType.SCINTILLATOR.value,
+                    "filters": [
+                        {
+                            "name": "Fermi GBM Bandpass",
+                            "id": uuid.UUID("81f8bd53-49f6-4a17-9395-86b87c2ac9e8"),
+                            "min_wavelength": GBM_WAVELENGTH_BANDPASS.min,
+                            "max_wavelength": GBM_WAVELENGTH_BANDPASS.max,
+                            "is_operational": True,
+                            "reference_url": "https://heasarc.gsfc.nasa.gov/docs/heasarc/missions/fermi.html",
+                        }
+                    ],
+                }
+            ],
+        },
+    ],
+    "ephemeris_types": [
+        {
+            "id": uuid.UUID("ad78414a-1643-4ff0-9ab0-403ea51b1067"),
+            "ephemeris_type": EphemerisType.TLE,
+            "priority": 1,
+            "parameters": {
+                "id": uuid.UUID("817a374a-5e60-4168-bb27-79a0df006c05"),
+                "norad_id": 33053,
+                "norad_satellite_name": "FGRST (GLAST)",
+            },
+        }
+    ],
+    "group": {
+        "id": uuid.UUID("2d1919c9-728b-4d8c-8afb-ad60c63eab4e"),
+        "name": "Fermi Gamma-ray Space Telescope",
+        "short_name": "Fermi",
+        "group_admin": {
+            "id": uuid.UUID("449c19ee-7b09-4b4d-ae52-99885a37f7be"),
+            "name": "Fermi Group Admin",
+        },
+    },
+}
+
+
+def upgrade() -> None:
+    # ### commands auto generated by Alembic - please adjust! ###
+    bind = op.get_bind()
+    session = orm.Session(bind=bind, expire_on_commit=False)
+
+    # create group based off the observatory
+    group = OBSERVATORY.pop("group")
+    group_admin = group.pop("group_admin")  # type: ignore
+    group_insert = Group(**group)  # type: ignore
+    session.add(group_insert)
+
+    # create group admin
+    permissions = (
+        session.query(Permission).filter(Permission.name.contains("group")).all()
+    )
+    group_admin_insert = GroupRole(
+        permissions=permissions, group=group_insert, **group_admin
+    )
+    session.add(group_admin_insert)
+
+    telescopes = OBSERVATORY.pop("telescopes")
+    ephemeris_types = OBSERVATORY.pop("ephemeris_types")
+
+    # create observatory
+    observatory_insert = Observatory(group=group_insert, **OBSERVATORY)
+    session.add(observatory_insert)
+    observatory_id = observatory_insert.id
+
+    # get the telescopes
+    for telescope in telescopes:  #  type:ignore
+        instruments = telescope.pop("instruments")
+        # create the telescope record
+        telescope_insert = Telescope(observatory_id=observatory_id, **telescope)
+        session.add(telescope_insert)
+        telescope_id = telescope_insert.id
+
+        # get the instruments
+        for instrument in instruments:
+            footprints = instrument.pop("footprint")
+            filters = instrument.pop("filters")
+
+            # create the instrument record
+            instrument_insert = Instrument(telescope_id=telescope_id, **instrument)
+            session.add(instrument_insert)
+            instrument_id = instrument_insert.id
+
+            for footprint in footprints:
+                # convert the input footprint to the string polygon
+                vertices = []
+                for point in footprint:
+                    vertices.append(ACROSSFootprintPoint(x=point["x"], y=point["y"]))
+
+                polygon = create_geography(polygon=vertices)
+
+                # create the footprint model record
+                footprint_insert = Footprint(
+                    polygon=polygon, instrument_id=instrument_id
+                )
+                session.add(footprint_insert)
+
+            for filter in filters:
+                filter_insert = Filter(instrument_id=instrument_id, **filter)
+                session.add(filter_insert)
+
+    # Add Ephemeris Parameters for Fermi
+    for ephemeris_type in ephemeris_types:  # type:ignore
+        # create the ephemeris type record
+        parameters = ephemeris_type.pop("parameters")
+        ephemeris_type_insert = ObservatoryEphemerisType(
+            observatory_id=observatory_id,
+            **ephemeris_type,
+        )
+        session.add(ephemeris_type_insert)
+
+        if ephemeris_type_insert.ephemeris_type == EphemerisType.TLE:
+            tle_parameters = TLEParameters(observatory_id=observatory_id, **parameters)
+            session.add(tle_parameters)
+
+        if ephemeris_type_insert.ephemeris_type == EphemerisType.JPL:
+            jpl_parameters = JPLEphemerisParameters(
+                observatory_id=observatory_id, **parameters
+            )
+            session.add(jpl_parameters)
+
+        if ephemeris_type_insert.ephemeris_type == EphemerisType.SPICE:
+            spice_parameters = SpiceKernelParameters(
+                observatory_id=observatory_id, **parameters
+            )
+            session.add(spice_parameters)
+
+        if ephemeris_type_insert.ephemeris_type == EphemerisType.GROUND:
+            ground_parameters = EarthLocationParameters(
+                observatory_id=observatory_id, **parameters
+            )
+            session.add(ground_parameters)
+
+    session.commit()
+    # ### end Alembic commands ###
+
+
+def downgrade() -> None:
+    # ### commands auto generated by Alembic - please adjust! ###
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+
+    group = session.scalar(select(Group).filter_by(name=OBSERVATORY["name"]))
+    observatory = session.scalar(
+        select(Observatory).filter_by(name=OBSERVATORY["name"])
+    )
+    session.delete(group)
+    session.delete(observatory)
+
+    tle_parameters = session.scalar(
+        select(TLEParameters).filter_by(observatory_id=observatory.id)  # type:ignore
+    )
+    if tle_parameters:
+        session.delete(tle_parameters)
+
+    jpl_parameters = session.scalar(
+        select(JPLEphemerisParameters).filter_by(
+            observatory_id=observatory.id  # type:ignore
+        )
+    )
+    if jpl_parameters:
+        session.delete(jpl_parameters)
+
+    spice_parameters = session.scalar(
+        select(SpiceKernelParameters).filter_by(
+            observatory_id=observatory.id  # type:ignore
+        )
+    )
+    if spice_parameters:
+        session.delete(spice_parameters)
+
+    ground_parameters = session.scalar(
+        select(EarthLocationParameters).filter_by(
+            observatory_id=observatory.id  # type:ignore
+        )
+    )
+    if ground_parameters:
+        session.delete(ground_parameters)
+
+    session.commit()
+    # ### end Alembic commands ###
