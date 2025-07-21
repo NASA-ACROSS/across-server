7923d1505b06378edd1fbcea9ef22acf9faf5175
swyatt7 <swyatt@email.arizona.edu>
2025-07-21 15:53:51 -0700

    craig's comment

--- across_server/routes/v1/filter/exceptions.py
+++ across_server/routes/v1/filter/exceptions.py
@@ -4,5 +4,5 @@ from ....core.exceptions import NotFoundException
 
 
 class FilterNotFoundException(NotFoundException):
-    def __init__(self, instrument_id: uuid.UUID):
-        super().__init__(entity_name="Filter", entity_id=instrument_id)
+    def __init__(self, filter_id: uuid.UUID):
+        super().__init__(entity_name="Filter", entity_id=filter_id)
