659e23dabe0b994fa89cd3538420a1896be483fb
Nitzan Frock <nitzan.n.frock@nasa.gov>
2025-08-28 11:25:13 -0700

    chore: add HIDE_LOCAL_ROUTE to local create_env_file

--- scripts/create_env_file.py
+++ scripts/create_env_file.py
@@ -13,17 +13,16 @@ def create_env_file() -> None:
         "ACROSS_DB_PWD": "local",
         "ACROSS_DB_NAME": "across",
         "ACROSS_DB_HOST": "localhost",
-        "ACROSS_DB_PORT": "5432",
+        "ACROSS_DB_PORT": 5432,
         "ACROSS_DB_ROLE": "aws-developer-role",
-        "ACROSS_DB_DEBUG": "False",
+        "ACROSS_DB_DEBUG": False,
         "ACROSS_ADMIN_TOKEN": "admin-token-local",
         "ACROSS_EMAIL": "nasa.across.dev@gmail.com",
         "ACROSS_EMAIL_PASSWORD": "acrossemailpassword",
         "ACROSS_EMAIL_HOST": "smtp.gmail.com",
-        "ACROSS_EMAIL_PORT": "465",
-        "SPACETRACK_USER": "spacetrack_user",
-        "SPACETRACK_PWD": "spacetrackpassword",
+        "ACROSS_EMAIL_PORT": 465,
         "ACROSS_DEBUG": True,
+        "HIDE_LOCAL_ROUTE": False,
     }
 
     if not os.path.exists(env_file_path):
