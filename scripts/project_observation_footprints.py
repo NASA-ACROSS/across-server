c262f737bcd42cba794f5ab056c7ce96067887b8
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2026-06-24 13:54:56 -0400

    feat: add logging and timing for insight while running

--- scripts/project_observation_footprints.py
+++ scripts/project_observation_footprints.py
@@ -4,7 +4,9 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import asyncio
+import time
 
+import structlog
 from geoalchemy2 import shape
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
@@ -12,10 +14,15 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from across_server.db import config, models
 from migrations.util import footprint_util
 
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
+
 MAX_RECORDS_PER_WRITE = 1000
 
 
 async def project_observation_footprints() -> None:
+    logger.info("Starting project_observation_footprints")
+    start_time = time.perf_counter()
+
     engine = create_async_engine(config.DB_URI)
 
     async_session = async_sessionmaker(engine, expire_on_commit=False)
@@ -28,7 +35,11 @@ async def project_observation_footprints() -> None:
 
         records = []
 
-        for instrument in instruments:
+        for index, instrument in enumerate(instruments, start=1):
+            logger.info(
+                f"({index}/{len(instruments)}) Gathering observations for {instrument.name}"
+            )
+
             # Get observations for this instrument with pointing coordinates
             # and no projected footprints
             obs_query = select(models.Observation).where(
@@ -46,6 +57,7 @@ async def project_observation_footprints() -> None:
                 x, y = poly.exterior.coords.xy  # type: ignore
                 detectors.append([{"x": x[i], "y": y[i]} for i in range(len(x))])
 
+            logger.info("Projecting footprints for %s observations", len(observations))
             for observation in observations:
                 # Project the instrument footprint to the pointing position and save it
                 projected_footprints = footprint_util.project_footprint(
@@ -72,10 +84,16 @@ async def project_observation_footprints() -> None:
                     )
                     records.append(observation_footprint)
 
+        logger.info("Committing to db")
         for i in range(0, len(records), MAX_RECORDS_PER_WRITE):
             chunk = records[i : i + MAX_RECORDS_PER_WRITE]
             session.add_all(chunk)
             await session.commit()
+
+        elapsed_seconds = time.perf_counter() - start_time
+        mins, secs = map(int, divmod(elapsed_seconds, 60))
+
+        logger.info(f"Done. Took: {mins} min {secs} sec")
 
     return None
 
