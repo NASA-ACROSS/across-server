66c53428eeeca4a62596e7a67a4314621322068a
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2026-07-23 13:26:13 -0400

    feat: limit pagination params with config, raise when pagination out of bounds for observation request total_count

--- across_server/routes/v1/observation/service.py
+++ across_server/routes/v1/observation/service.py
@@ -383,6 +383,15 @@ class ObservationService:
         )
         total_count = (await self.db.execute(count_query)).scalar_one()
 
+        # Raise when page requests out of bounds of requested data length
+        if data.page and data.page_limit:
+            request_total_data_start = data.page * data.page_limit
+
+            if total_count < request_total_data_start:
+                raise InvalidObservationReadParametersException(
+                    "Page and limit selections out of range"
+                )
+
         # query to find the ids quickly with indexes and leaf info
         nested_id_subq = (
             select(models.Observation.id)
